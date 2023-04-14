0188101d992fd64c4f47d5de8d0904ce00e79410
Nursultan Zhanybekov <121675952+arnursultan@users.noreply.github.com>
2023-04-14 19:25:03 +0600

    Пятый урок

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,61 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 from blog.models import Post
 
 
-def get_index(request):
-    posts = Post.objects.all()
-    context = {
-        "title": "Главная страница",
-        "posts": posts,
-    }
-    return render(request, "blog/index.html", context=context)
+# Read/Retrieve
+class IndexView(generic.ListView):
+    # model = Post
+    queryset = Post.objects.filter(status=True)
+    context_object_name = "posts"
+    template_name = "blog/index.html"
+    # extra_context = {"title": "Главная страница"}
+
+
+# Read/Retrieve
+class PostDetailView(generic.DetailView):
+    model = Post
+    context_object_name = "post"
+    template_name = "blog/post_detail.html"
+
+
+# CREATE
+class PostCreateView(generic.CreateView):
+    model = Post
+    template_name = "blog/post_create.html"
+    success_url = reverse_lazy("index-page")
+    fields = ["title", "content"]
+
+
+# DELETE
+class PostDeleteView(generic.DeleteView):
+    model = Post
+    success_url = reverse_lazy("index-page")
+
+
+# UPDATE
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    template_name = "blog/post_update.html"
+    success_url = reverse_lazy("index-page")
+    fields = ["title", "content"]
+
+
+# def get_index(request):
+#     posts = Post.objects.all()
+#     context = {
+#         "title": "Главная страница",
+#         "posts": posts,
+#     }
+#     return render(request, "blog/index.html", context=context)
 
 
 def get_about(request):
-    context = {
-        "title": "Страница о нас"
-    }
-    return render(request, "blog/about.html", context=context)
+    # context = {
+    #     "title": "Страница о нас"
+    # }
+    return render(request, "blog/about.html", context=None)
 
 
 def get_contacts(request):
@@ -25,23 +65,18 @@ def get_contacts(request):
     return render(request, "blog/contacts.html", context=context)
 
 
-def get_post(request, pk):
-    post = Post.objects.get(id=pk)
-    context = {
-        "post": post,
-    }
-    return render(request, "blog/post_detail.html", context)
+# Read\Retrieve
+# def get_post(request, pk):
+#     post = Post.objects.get(id=pk)
+#     context = {
+#         "post": post,
+#     }
+#     return render(request, "blog/post_detail.html", context)
 
 
-def post_create(request):
-    context = {
-        "title": ""
-    }
-    return render(request, 'blog/post_create.html', context=context)
-
-
-def post_update(request):
-    context = {
-        "title": ""
-    }
-    return render(request, 'blog/post_update.html', context=context)
+# def post_update(request):
+#     return render(request, "blog/post_update.html")
+#
+#
+# def post_create(request):
+#     return render(request, "blog/post_create.html")
